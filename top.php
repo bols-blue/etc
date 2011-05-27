ac9d9520595ae67fe8a1a4c5e3f30a3d70c4779e
bols-blue <bols@localhost.localdomain>
2011-05-27 16:59:55 +0900

    commit chage

---
+++ top.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php -e
+<?php
+error_reporting(E_ALL);
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+require_once('PomGenerator/class.PomGenerator.php');
+
+
+
+?>
